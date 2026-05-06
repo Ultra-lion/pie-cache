7fb2ae9f285bc1e7f8eb6a04586a042485bf74f0
Ultra-Lion <rohanakhtar1999@gmail.com>
2026-05-06 21:00:05 +0500

    fix

--- socket_handler.py
+++ socket_handler.py
@@ -10,38 +10,43 @@ SOCKET_SERVER_PORT = 9876
 async def cache_worker(reader, writer):
     
     global PieCacheManagerInstance
-
-    data = await reader.read(100)
-    message = data.decode()
     addr = writer.get_extra_info('peername')
+    try:
+        while True:
+            data = await reader.read(1024)
+            message = data.decode()
 
-    pat = '(get|put|del) ([A-Za-z0-9].*)'
-    match = re.search(pat, message)
-    res=""
-    if match:
-        query_items = match.groups()
-        op = query_items[0]
-        if op=='get':
-            key = query_items[1]
-            res = PieCacheManagerInstance.get_cache_item(key)
-        elif op=='put':
-            key, val = query_items[1].split(' ')
-            res = PieCacheManagerInstance.set_cache_item(key,val)
-        elif op=='del':
-            key = query_items[1]
-            PieCacheManagerInstance.del_item_cache(key)
-            res = "Done"
+            pat = '(get|put|del) ([A-Za-z0-9].*)'
+            match = re.search(pat, message)
+            res=""
+            if match:
+                query_items = match.groups()
+                op = query_items[0]
+                if op=='get':
+                    key = query_items[1]
+                    res = PieCacheManagerInstance.get_cache_item(key)
+                elif op=='put':
+                    key, val = query_items[1].split(' ')
+                    res = PieCacheManagerInstance.set_cache_item(key,val)
+                elif op=='del':
+                    key = query_items[1]
+                    PieCacheManagerInstance.del_item_cache(key)
+                    res = "Done"
 
 
-        writer.write(res.encode())
+                writer.write(res.encode())
 
-    else:
-        print("what the helly")
-        writer.write("Invalid Query")
+            else:
+                print("what the helly")
+                writer.write("Invalid Query")
 
-    await writer.drain()
-    writer.close()
-    await writer.wait_closed()
+                await writer.drain()
+    except ConnectionResetError:
+        print(f"Connection Reset by client {addr}")
+    finally:
+        print(f"closing connection for {addr}")
+        writer.close()
+        await writer.wait_closed()
 
 
 
